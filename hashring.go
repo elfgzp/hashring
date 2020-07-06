60fc6040c746cd2016766682e0ee91dab6d181fd
Gzp_ <741424975@qq.com>
2020-07-06 21:34:29 +0800

    [MOD] 修改 hashring

--- hashring.go
+++ hashring.go
@@ -1,49 +1,142 @@
 package hashring
 
+import (
+	"errors"
+	"fmt"
+	"hash/adler32"
+	"sort"
+)
+
+// Node 节点
+type Node struct {
+	Name   string
+	Weight int
+}
+
+// VirtualNode 虚拟节点
+type VirtualNode struct {
+	Node
+	VirtualName string
+	HashValue   uint32
+}
+
+// virtualNodeArray 节点列表
+type virtualNodeArray []*VirtualNode
+
+// Len Len
+func (vn virtualNodeArray) Len() int {
+	return len(vn)
+}
+
+// Less Less
+func (vn virtualNodeArray) Less(i, j int) bool {
+	return vn[i].HashValue < vn[j].HashValue
+}
+
+// Swap Swap
+func (vn virtualNodeArray) Swap(i, j int) {
+	vn[i], vn[j] = vn[j], vn[i]
+}
+
+// Sort Sort
+func (vn virtualNodeArray) Sort() {
+	sort.Sort(vn)
+}
+
 // HashRing 哈希环
-type HashRing struct{}
+type HashRing struct {
+	VirtualNodeNum int
+	RealNodeMap    map[string]*Node
+	virtualNodeArr virtualNodeArray
+}
+
+func (h *HashRing) generate() {
+	var totalWeight int
+	h.virtualNodeArr = make(virtualNodeArray, 0)
+
+	for _, node := range h.RealNodeMap {
+		totalWeight += node.Weight
+	}
+
+	if totalWeight == 0 {
+		return
+	}
+
+	adl := adler32.New()
+	for _, node := range h.RealNodeMap {
+		virtualNodeNum := node.Weight * h.VirtualNodeNum / totalWeight
+		for i := 1; i <= virtualNodeNum; i++ {
+			virtualNode := &VirtualNode{}
+			virtualNode.Name = node.Name
+			virtualNode.Weight = node.Weight
+			virtualNode.VirtualName = fmt.Sprintf("%s#%d", node.Name, i)
+			adl.Write([]byte(virtualNode.VirtualName))
+			virtualNode.HashValue = adl.Sum32()
+			adl.Reset()
+			h.virtualNodeArr = append(h.virtualNodeArr, virtualNode)
+		}
+	}
+
+	h.virtualNodeArr.Sort()
+}
 
 // NewHashRing 新建哈希环
-func NewHashRing() (*HashRing, error) {
-	hashRing := &HashRing{}
+func NewHashRing(virtualNodeNum int) (*HashRing, error) {
+
+	if virtualNodeNum == 0 {
+		virtualNodeNum = 150
+	}
+
+	hashRing := &HashRing{
+		VirtualNodeNum: virtualNodeNum,
+		RealNodeMap:    make(map[string]*Node, 0),
+	}
 
 	return hashRing, nil
 }
 
 // AddNode 增加节点
-func (h *HashRing) AddNode(nodeName string) error {
-	return nil
+func (h *HashRing) AddNode(nodeName string, nodeWeight int) {
+	node := &Node{
+		Name:   nodeName,
+		Weight: nodeWeight,
+	}
+	h.RealNodeMap[nodeName] = node
+	h.generate()
 }
 
 // RemoveNode 删除节点
-func (h *HashRing) RemoveNode(nodeName string) error {
-	return nil
+func (h *HashRing) RemoveNode(nodeName string) {
+	delete(h.RealNodeMap, nodeName)
+	h.generate()
 }
 
-// GetNode 获取节点
-func (h *HashRing) GetNode(key string) string {
-	return "nodeName"
+// NodeLoadBalance 节点负载均衡
+func (h *HashRing) NodeLoadBalance(key string) (string, error) {
+	if len(h.virtualNodeArr) == 0 {
+		return "", errors.New("No available node. ")
+	}
+
+	adl := adler32.New()
+	adl.Write([]byte(key))
+	hashValue := adl.Sum32()
+
+	i := sort.Search(h.virtualNodeArr.Len(), func(i int) bool {
+		return h.virtualNodeArr[i].HashValue >= hashValue
+	})
+
+	if i == h.virtualNodeArr.Len() {
+		i = 0
+	}
+
+	virtualNode := h.virtualNodeArr[i]
+
+	return virtualNode.Name, nil
 }
 
 // AddNodes 批量增加节点
-func (h *HashRing) AddNodes(nodeNameArr []string) error {
-	var err error
-
-	for _, nodeName := range nodeNameArr {
-		err = h.AddNode(nodeName)
-		if err != nil {
-			break
-		}
+func (h *HashRing) AddNodes(nodeWeightMap map[string]int) {
+	for nodeName, nodeWeight := range nodeWeightMap {
+		h.AddNode(nodeName, nodeWeight)
 	}
-
-	if err != nil {
-		for _, nodeName := range nodeNameArr {
-			err = h.RemoveNode(nodeName)
-			if err != nil {
-				break
-			}
-		}
-	}
-
-	return err
 }
